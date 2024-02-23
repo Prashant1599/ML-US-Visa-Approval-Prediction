8433fd4de1070f96c1bb923a168709f8c7315400
Frio1599 <111003003+Frio1599@users.noreply.github.com>
2024-02-23 22:00:23 +0530

    exc & log

---
+++ demo.py
@@ -0,0 +1,8 @@
+from US_Visa.logger import logging
+from US_Visa.exception import USvisaException
+import sys
+
+try:
+    a = 1/ "10"
+except Exception as e:
+    raise USvisaException(e, sys) from e
